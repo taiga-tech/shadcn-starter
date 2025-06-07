d0e1ad5e4c774b794cc81dad00765dd7a706815d
taiga-tech <taiga.dev.n@gmail.com>
2025-06-08 02:12:07 +0900

    Add linting configuration for TypeScript and JavaScript files
    - Updated the lint task to depend on a new linting step for various file types.
    - Introduced a new linting step that includes TypeScript, JavaScript, and other relevant file extensions while excluding node_modules and build directories.

--- apps/docs/eslint.config.ts
+++ apps/docs/eslint.config.ts
@@ -1,4 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import { nextJsConfig } from '@workspace/eslint-config/next'
+import storybook from 'eslint-plugin-storybook'
 
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig
+/** @type {import("eslint").Linter.Config[]} */
+export default [...nextJsConfig, ...storybook.configs['flat/recommended']]
--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 /**
  * A shared ESLint configuration for the repository.
  *
- * @type {import("eslint").Linter.Config}
+ * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
     js.configs.recommended,
