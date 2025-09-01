99e9bf17f248fb8dbd04ed40269399dc8fb8d74e
taiga-tech <taiga.dev.n@gmail.com>
2025-09-01 23:50:22 +0900

    fix: jest設定ファイルでConfigのインポートを修正し、package.jsonのスクリプトを更新

--- packages/jest-config/next.ts
+++ packages/jest-config/next.ts
@@ -1,7 +1,6 @@
 import nextJest from 'next/jest'
 
 // unfortunately, need to disambiguate the `Config` namespace @jest/types uses (via next/jest) and the `Config` type we want for typing our config here
-import type { Config as ConfigNamespace } from '@jest/types'
 import type { Config } from 'jest'
 
 import { config as baseConfig } from './base'
